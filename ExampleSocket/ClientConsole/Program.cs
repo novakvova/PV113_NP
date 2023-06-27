72d08e22d85125119fca687f306497ac0524af04
novakvova <novakvova@gmail>
2023-06-27 20:03:17 +0300

    Begin working socket

---
+++ ExampleSocket/ClientConsole/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //наш сервер, який буде отримувать повідомлення
+            IPAddress serverIP = IPAddress.Parse("127.0.0.1");
+            int port = 9076;
+            try
+            {
+                //ідентифікація сервер ip+port
+                IPEndPoint endPoint = new IPEndPoint(serverIP, port);
+                Socket client = new Socket(AddressFamily.InterNetwork, 
+                    SocketType.Stream, ProtocolType.Tcp);
+                //підключення до сервера
+                client.Connect(endPoint);
+                Console.Write("Вкажіть повідомлення->_");
+                string message = Console.ReadLine();
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                client.Send(data); //відправляємо повідомелння в байтах
+                //очікуємо відповіді від сервера
+                data = new byte[1024];
+                int bytes = 0; //розмір повідомлення від сервера
+                do
+                {
+                    bytes = client.Receive(data); //отримує інформацію від сервера
+                    Console.WriteLine("Нам приалав сервер {0}", Encoding.Unicode.GetString(data));
+
+                } while (client.Available > 0);
+                client.Shutdown(SocketShutdown.Both); //завершуємо зяднання із сервером
+                client.Close(); //закриваємо повне зяднання
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Проблема при рооті із сервером {0}", ex.ToString());
+            }
+        }
+    }
+}
---
+++ ExampleSocket/ServerConsole/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerConsole
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //IP - адреса нашого ПК
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            int port = 9076; //порт на якому працює наш сервер
+            //ідертифікатор нашого сервера
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+            //забезпучує роботу на основі протоколу Tcp
+            Socket server = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Bind(endPoint); //сокет буде слухати запити по даноій адресі і порту
+                server.Listen(10); //починаємо прослуховування запитів
+                Console.WriteLine("Сервер запущено {0}", endPoint);
+                while (true)
+                {
+                    //очікуємо запит від клієнта
+                    Socket client = server.Accept(); //якийсь клієнт прислав нам запит
+                    int bytes = 0; //кількість байт
+                    byte[] data = new byte[1024]; //масив збірігає повідомлення клієнта
+                    //Читаємо повідомлення клієнта
+                    do
+                    {
+                        bytes = client.Receive(data); //читаємо байти від клієнта
+                        Console.WriteLine("Повідомлення {0}", Encoding.Unicode.GetString(data));
+                    } while (client.Available > 0); //якщо кількість байт, які ми читаємо більші 0
+
+                    string message = "Дякую. Сервер отримав ваше повідомлення.";
+                    data = Encoding.Unicode.GetBytes(message); //перетворюємо повідомлення в байти
+                    client.Send(data); //сервер відправляє клієнту інформацію
+                    client.Shutdown(SocketShutdown.Both); //завершуємо роботу із клієнтом
+                    client.Close(); //повністю видаляємо зяднання
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Помилка роботи програми {0}", ex.Message);
+            }
+        }
+    }
+}
